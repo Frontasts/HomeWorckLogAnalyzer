31a5e3157e36112c7aec9ed24017c60ffb267a86
Frontasts <steamfronts0110@gmail.com>
2026-09-11 14:28:35 +0300

    Добавлен конструктор с полями для инициализации LogAnalyzer и ключевых слов

--- HomeWorckLogAnalyzer/Application.cs
+++ HomeWorckLogAnalyzer/Application.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,26 @@ namespace HomeWorckLogAnalyzer
 {
     public class Application
     {
+        private LogAnalyzer _analyzer;
+        private List<string> _keywords;
+
+        public Application()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string logPath = Path.Combine(baseDir, "server.log");
+
+            _analyzer = new LogAnalyzer(logPath);
+
+            _keywords = new List<string>
+            {
+                "ERROR",
+                "WARNING",
+                "INFO",
+                "DEBUG",
+                "CRITICAL"
+            };
+        }
+
         public void Run()
         {
 
