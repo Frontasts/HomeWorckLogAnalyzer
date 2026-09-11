5a317e353642f83454cfd337783728a2001521b6
Frontasts <steamfronts0110@gmail.com>
2026-09-11 14:23:29 +0300

    Создан класс LogAnalyzer для чтения у правление логами

---
+++ HomeWorckLogAnalyzer/LogAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckLogAnalyzer
+{
+    public class LogAnalyzer
+    {
+        private string _logFilePath;
+
+        public LogAnalyzer(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+    }
+}
